cf50f50edcabb7b176e93e1049137969ef5b4a6d
wishon1 <wisdomhonest1@gmail.com>
2024-01-04 14:23:04 -0500

    measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,14 +6,26 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0, tree_height;
+
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		left_height = 1 + binary_tree_height(tree->left);
+	else
+		left_height = 0;
 
-	/* Return d max of left_height nd right_height + 1 for the current node */
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_height(tree->right);
+	else
+		right_height = 0;
+
+	if ( left_height > right_height)
+		tree_height = left_height;
+	else
+		tree_height = right_height;
+	
+	return (tree_height);
 }
